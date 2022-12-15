16d2aecc15c655403b360fb1d0608ee49681bf63
Hitesha Ukey <hitesha.ukey@gmail.com>
2022-12-15 11:27:44 -0500

    rug

--- src/components/objects/Extras/Extras.js
+++ src/components/objects/Extras/Extras.js
@@ -1,8 +1,11 @@
-import { Group } from 'three';
+import { Group, TextureLoader, MeshPhysicalMaterial } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import LAUNDRY_BASKET from './laundry.gltf';
 require('./laundry.bin');
 require('./laundry_textures/material_0_baseColor.jpeg');
+import RUG from './rug.gltf';
+require('./rug.bin');
+const pngPath = require('./pattern.png');
 
 
 class Extras extends Group {
@@ -19,6 +22,22 @@ class Extras extends Group {
             gltf.scene.rotateY(Math.PI*3/2)
             gltf.scene.translateY(0.4);
             gltf.scene.translateX(1.6);
+        });
+
+        const texture = new TextureLoader().load(pngPath);
+
+        loader.load(RUG, (gltf) => {
+            gltf.scene.traverse( function(object) {
+                if ( object.isMesh ) {
+                    object.material.dispose();
+                    object.material = new MeshPhysicalMaterial( { map: texture } );
+                    object.scale.set(0.1, 0.1, 0.1);
+                }
+            } );
+            this.add(gltf.scene);
+            // gltf.scene.rotateY(Math.PI*3/2)
+            gltf.scene.translateY(0.05);
+            gltf.scene.translateX(1.2);
         });
     }
 }
